be6d47d44607d595248d385ec5c72b25558143c2
Bartek Noga <barteknoga14@gmail.com>
2022-08-31 21:07:30 +0200

    Deleted colors.js

--- components/ColorButton.js
+++ components/ColorButton.js
@@ -1,5 +1,5 @@
 import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { Colors } from '../constants/colors'
+
 
 function ColorButton({ onSelect, selected, color }) {
     return (
